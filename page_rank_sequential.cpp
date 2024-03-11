d549832a4b053ef0d7521d0bf697de48480c72dc
Vitolus <vito.davide00@gmail.com>
2024-03-11 16:06:54 +0100

    refactor

--- page_rank_sequential.cpp
+++ page_rank_sequential.cpp
@@ -1,61 +1,61 @@
 #include "page_rank_sequential.h"
 
 page_rank_sequential::page_rank_sequential(const string &filename) : filename(filename){
-    ifstream file(filename);
-    if(!file.is_open()){
-        cout << "file not found" << endl;
-        return;
-    }
-    string line;
-    for (int i = 0; i < 2; ++i) {
-        getline(file, line); // skip first two lines
-    }
-    getline(file, line); // line with number of nodes and edges
-    istringstream iss(line);
-    string n_nodes;
-    while(iss >> n_nodes){
-        if(n_nodes == "Nodes:"){
-            iss >> n_nodes;
-            break;
-        }
-    }
-    file.close();
-    uint dim = stoi(n_nodes);
-    matrix.resize(dim, vector<int>(dim, 0));
-    z_order.resize(dim*dim, 0);
+	ifstream file(filename);
+	if(!file.is_open()){
+		cout << "file not found" << endl;
+		return;
+	}
+	string line;
+	for(int i = 0; i < 2; ++i){
+		getline(file, line); // skip first two lines
+	}
+	getline(file, line); // line with number of nodes and edges
+	istringstream iss(line);
+	string n_nodes;
+	while(iss >> n_nodes){
+		if(n_nodes == "Nodes:"){
+			iss >> n_nodes;
+			break;
+		}
+	}
+	file.close();
+	uint dim = stoi(n_nodes);
+	matrix.resize(dim, vector<int>(dim, 0));
+	z_order.resize(dim * dim, 0);
 }
 
-auto page_rank_sequential::interleave(u_int16_t x, u_int16_t y) {
-    static const u_int16_t M[] = {0x5555, 0x3333, 0x0F0F, 0x00FF};
-    static const u_int16_t S[] = {1, 2, 4, 8};
-    x = (x | (x << S[3])) & M[3];
-    x = (x | (x << S[2])) & M[2];
-    x = (x | (x << S[1])) & M[1];
-    x = (x | (x << S[0])) & M[0];
-    y = (y | (y << S[3])) & M[3];
-    y = (y | (y << S[2])) & M[2];
-    y = (y | (y << S[1])) & M[1];
-    y = (y | (y << S[0])) & M[0];
-    auto result = x | (y << 1);
-    return result;
+auto page_rank_sequential::interleave(u_int16_t x, u_int16_t y){
+	static const u_int16_t M[] = {0x5555, 0x3333, 0x0F0F, 0x00FF};
+	static const u_int16_t S[] = {1, 2, 4, 8};
+	x = (x | (x << S[3])) & M[3];
+	x = (x | (x << S[2])) & M[2];
+	x = (x | (x << S[1])) & M[1];
+	x = (x | (x << S[0])) & M[0];
+	y = (y | (y << S[3])) & M[3];
+	y = (y | (y << S[2])) & M[2];
+	y = (y | (y << S[1])) & M[1];
+	y = (y | (y << S[0])) & M[0];
+	auto result = x | (y << 1);
+	return result;
 }
 
-void page_rank_sequential::parse_dataset() {
-    ifstream file(filename);
-    if(!file.is_open()){
-        cout << "file not found" << endl;
-        return;
-    }
-    string line;
-    for (int i = 0; i < 4; ++i) {
-        getline(file, line); // skip header
-    }
-    while (std::getline(file, line)) {
-        std::istringstream iss(line);
-        int row, col;
-        iss >> col >> row;
-        matrix[row][col] = 1;
-        z_order[interleave(row, col)] = 1;
-    }
-    file.close();
+void page_rank_sequential::parse_dataset(){
+	ifstream file(filename);
+	if(!file.is_open()){
+		cout << "file not found" << endl;
+		return;
+	}
+	string line;
+	for(int i = 0; i < 4; ++i){
+		getline(file, line); // skip header
+	}
+	while(getline(file, line)){
+		istringstream iss(line);
+		int row, col;
+		iss >> col >> row;
+		matrix[row][col] = 1;
+		z_order[interleave(row, col)] = 1;
+	}
+	file.close();
 }

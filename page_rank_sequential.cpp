6c86c6855295d4d27a09c5428842aafb4aa9ff20
Vitolus <vito.davide00@gmail.com>
2024-03-17 19:04:53 +0100

    some fixes

--- page_rank_sequential.cpp
+++ page_rank_sequential.cpp
@@ -1,26 +1,39 @@
 #include "page_rank_sequential.h"
 
-auto page_rank_sequential::interleave(u_int16_t x, u_int16_t y){
+auto page_rank_sequential::interleave(u_int16_t col, u_int16_t row){
 	static const u_int16_t M[] = {0x5555, 0x3333, 0x0F0F, 0x00FF};
 	static const u_int16_t S[] = {1, 2, 4, 8};
-	x = (x | (x << S[3])) & M[3];
-	x = (x | (x << S[2])) & M[2];
-	x = (x | (x << S[1])) & M[1];
-	x = (x | (x << S[0])) & M[0];
-	y = (y | (y << S[3])) & M[3];
-	y = (y | (y << S[2])) & M[2];
-	y = (y | (y << S[1])) & M[1];
-	y = (y | (y << S[0])) & M[0];
-	auto result = x | (y << 1);
+	col = (col | (col << S[3])) & M[3];
+	col = (col | (col << S[2])) & M[2];
+	col = (col | (col << S[1])) & M[1];
+	col = (col | (col << S[0])) & M[0];
+	row = (row | (row << S[3])) & M[3];
+	row = (row | (row << S[2])) & M[2];
+	row = (row | (row << S[1])) & M[1];
+	row = (row | (row << S[0])) & M[0];
+	auto result = col | (row << 1);
 	return result;
 }
 
-float page_rank_sequential::out_degree(int row){
-	float degree = 0;
-	for(auto &col : graph[row]){
-		if(col != 0){
-			++degree;
-		}
+void page_rank_sequential::deinterleave(u_int32_t z, u_int16_t &col, u_int16_t &row) {
+	static const u_int16_t M[] = {0x5555, 0x3333, 0x0F0F, 0x00FF};
+	static const u_int16_t S[] = {0, 1, 2, 4};
+	col = z;
+	row = z >> 1;
+	col = (col | (col >> S[3])) & M[3];
+	col = (col | (col >> S[2])) & M[2];
+	col = (col | (col >> S[1])) & M[1];
+	col = (col | (col >> S[0])) & M[0];
+	row = (row | (row >> S[3])) & M[3];
+	row = (row | (row >> S[2])) & M[2];
+	row = (row | (row >> S[1])) & M[1];
+	row = (row | (row >> S[0])) & M[0];
+}
+
+double page_rank_sequential::out_degree(vector<vector<double>> &graph, int col){
+	double degree = 0.0;
+	for(const auto &row : graph){
+		degree += row[col];
 	}
 	return degree;
 }
@@ -33,48 +46,50 @@ page_rank_sequential::page_rank_sequential(const string &filename) : filename(fi
 	}
 	string line;
 	for(short i = 0; i < 2; ++i){
-		getline(file, line); // skip first two lines
+		getline(file, line); // skip first two header lines
 	}
 	getline(file, line); // line with number of nodes and edges
 	istringstream iss(line);
 	string n_nodes;
 	while(iss >> n_nodes){
 		if(n_nodes == "Nodes:"){
-			iss >> n_nodes;
+			iss >> n_nodes;  // get number of nodes
 			break;
 		}
 	}
 	getline(file, line); // skip last header line
 	uint dim = stoi(n_nodes);
-	graph.resize(dim, vector<float>(dim, 0.0));
+
+	/// initialize graph with 1 where there is an edge
+	vector<vector<double>> graph(dim, vector<double>(dim, 0.0));
 	while(getline(file, line)){
 		iss.str(line);
-		int row, col;
-		iss >> row >> col;
-		graph[row][col] = 1;
+		int start, arrive;
+		iss >> start >> arrive;
+		graph[arrive][start] = 1;
 	}
 	file.close();
-	contributions.resize(dim, vector<float>(dim, 0.0));
-	z_order.resize(dim * dim, 0.0);
+
 	rank.resize(dim, 1.0/dim);
+	/// initialize z_order with 1/out_degree or 1/dim
+	z_order.resize(dim*dim, 0.0);
 	for(int i = 0; i < dim; ++i){
 		for(int j = 0; j < dim; ++j){
-			float oj = out_degree(j);
-			if(graph[j][i] == 1){
-				contributions[i][j] = 1.0/oj;
-				z_order[interleave(i, j)] = 1.0/oj;
-			}else if(oj == 0){
-				contributions[i][j] = 1.0/dim;
-				z_order[interleave(i, j)] = 1.0/dim;
+			double oj = out_degree(graph, j);
+			if(graph[i][j] == 1){
+				z_order[interleave(j, i)] = 1.0/oj;
+			}
+			else if(oj == 0){
+				z_order[interleave(j, i)] = 1.0/dim;
 			}
 		}
 	}
 }
 
-vector<float> page_rank_sequential::compute_page_rank(float beta){
+vector<double> page_rank_sequential::compute_page_rank(double beta){
 	uint dim = rank.size();
-	vector<float> results(dim, 0.0);
-	float c = (1.0 - beta) / dim;
+	vector<double> results(dim, 0.0);
+	double c = (1.0 - beta) / dim;
 	for(int i = 0; i < dim; ++i){
 		for(int j = 0; j < dim; ++j){
 			results[i] += beta * z_order[interleave(i, j)] * rank[j];
--- page_rank_sequential.h
+++ page_rank_sequential.h
@@ -11,13 +11,14 @@ using namespace std;
 
 class page_rank_sequential{
     const string &filename;
-    vector<vector<float>> graph, contributions;
-    vector<float> z_order, rank;
+    vector<double> z_order, rank;
+	vector<pair<u_int16_t, u_int16_t>> z_order_pos;
     static auto interleave(u_int16_t, u_int16_t);
-	float out_degree(int);
+	static void deinterleave(u_int32_t, u_int16_t &, u_int16_t &);
+	static double out_degree(vector<vector<double>> &, int);
 public:
     explicit page_rank_sequential(const string &);
-	vector<float> compute_page_rank(float);
+	vector<double> compute_page_rank(double);
 };
 
 #endif //LEARN_ASSIGNMENT1_PAGE_RANK_SEQUENTIAL_H
